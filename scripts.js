afaf14bd873fa50c5e3c5c224ede274a3ce1430c
Arthur-Byamukama <48201055+Arthur-Byamukama@users.noreply.github.com>
2019-03-26 16:34:39 +0300

    Add files via upload
    2nd calculator commit

---
+++ scripts.js
@@ -0,0 +1,101 @@
+$(document).ready (function(){
+  $("#nine").click(function(){
+    $(this).toggle("hide")
+  })
+});
+
+$(document).ready (function(){
+  $("#eight").click(function(){
+    $(this).toggle("hide")
+  })
+});
+
+$(document).ready (function(){
+  $("#seven").click(function(){
+    $(this).toggle("hide")
+  })
+});
+
+$(document).ready (function(){
+  $("#six").click(function(){
+    $(this).toggle("hide")
+  })
+});
+
+$(document).ready (function(){
+  $("#five").click(function(){
+    $(this).toggle("hide")
+  })
+});
+
+$(document).ready (function(){
+  $("#four").click(function(){
+    $(this).toggle("hide")
+  })
+});
+
+$(document).ready (function(){
+  $("#three").click(function(){
+    $(this).toggle("hide")
+  })
+});
+
+$(document).ready (function(){
+  $("#two").click(function(){
+    $(this).toggle("hide")
+  })
+});
+
+$(document).ready (function(){
+  $("#one").click(function(){
+    $(this).toggle("hide")
+  })
+});
+
+$(document).ready (function(){
+  $("#plus").click(function(){
+    $(this).toggle("hide")
+  })
+});
+
+$(document).ready (function(){
+  $("#minus").click(function(){
+    $(this).toggle("hide")
+  })
+});
+
+$(document).ready (function(){
+  $("#multiplication").click(function(){
+    $(this).toggle("hide")
+  })
+});
+
+$(document).ready (function(){
+  $("#equals").click(function(){
+    $(this).toggle("hide")
+  })
+});
+
+$(document).ready (function(){
+  $("#del").click(function(){
+    $(this).toggle("hide")
+  })
+});
+
+$(document).ready (function(){
+  $("#zero").click(function(){
+    $(this).toggle("hide")
+  })
+});
+
+$(document).ready (function(){
+  $("#percent").click(function(){
+    $(this).toggle("hide")
+  })
+});
+
+$(document).ready (function(){
+  $("#divide").click(function(){
+    $(this).toggle("hide")
+  })
+});
